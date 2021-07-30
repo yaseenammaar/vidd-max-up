1d1ce66ec63df2bccee7ab8dd37f84ef45c43853
yaseenammaar <yammaar9@gmail.com>
2021-07-30 21:04:54 +0530

    Modular

---
+++ addtext.php
@@ -0,0 +1,53 @@
+<script src="https://demos.jquerymobile.com/1.4.2/js/jquery.mobile-1.4.2.min.js"></script>
+            <div class="page-breadcrumb bg-white">
+                <div class="row align-items-center">
+                    <div class="col-lg-3 col-md-4 col-sm-4 col-xs-12">
+                        <h4 class="page-title">Step 4 - Add Texts </h4>
+                    </div>
+                    <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
+                        <div class="d-md-flex">
+                            <ol class="breadcrumb ms-auto">
+                                <li><a href="#" class="fw-normal">Dashboard</a></li>
+                            </ol>
+                           
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            <div class="container-fluid">
+
+
+                <style type="text/css">
+                    
+                </style>
+                <div class="row">
+                    <div class="col-md-12 col-lg-12 col-sm-12 col-xs-12">
+                        <div class="white-box">
+                            
+                            <center>
+                                <img style="width: 100%;padding: 0px 60px 0px 60px" src="https://www.adorama.com/alc/wp-content/uploads/2018/11/landscape-photography-tips-yosemite-valley-feature.jpg">
+                            </center>
+                            
+                            <form>
+                                <div data-role="rangeslider">
+                                    <input type="range" name="range-1a" id="range-1a" min="0" max="100" value="0" data-popup-enabled="true" data-show-value="true">
+                                    <input type="range" name="range-1b" id="range-1b" min="0" max="100" value="100" data-popup-enabled="true" data-show-value="true">
+                                </div>
+                            </form>
+
+                </div>
+                <!-- ============================================================== -->
+                <!-- RECENT SALES -->
+                <!-- ============================================================== -->
+                
+                    <!-- /.col -->
+                </div>
+            </div>
+            <!-- ============================================================== -->
+            <!-- End Container fluid  -->
+            <!-- ============================================================== -->
+            <!-- ============================================================== -->
+            <!-- footer -->
+            <!-- ============================================================== -->
+            
