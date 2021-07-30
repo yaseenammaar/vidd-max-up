3f47f30594538435b85db08a0f193878c1d8b834
yaseenammaar <yammaar9@gmail.com>
2021-07-30 20:06:06 +0530

    Code

---
+++ js/custom.js
@@ -0,0 +1,69 @@
+$(function() {
+    "use strict";
+
+    $(".preloader").fadeOut();
+    // this is for close icon when navigation open in mobile view
+    $(".nav-toggler").on('click', function() {
+        $("#main-wrapper").toggleClass("show-sidebar");
+        $(".nav-toggler i").toggleClass("ti-menu");
+    });
+    $(".search-box a, .search-box .app-search .srh-btn").on('click', function() {
+        $(".app-search").toggle(200);
+        $(".app-search input").focus();
+    });
+
+    // ============================================================== 
+    // Resize all elements
+    // ============================================================== 
+    $("body, .page-wrapper").trigger("resize");
+    $(".page-wrapper").delay(20).show();
+    
+    //****************************
+    /* This is for the mini-sidebar if width is less then 1170*/
+    //**************************** 
+    var setsidebartype = function() {
+        var width = (window.innerWidth > 0) ? window.innerWidth : this.screen.width;
+        if (width < 1170) {
+            $("#main-wrapper").attr("data-sidebartype", "mini-sidebar");
+        } else {
+            $("#main-wrapper").attr("data-sidebartype", "full");
+        }
+    };
+    $(window).ready(setsidebartype);
+    $(window).on("resize", setsidebartype);
+
+});
+
+var fileobj;
+function upload_file(e) {
+    e.preventDefault();
+    fileobj = e.dataTransfer.files[0];
+    ajax_file_upload(fileobj);
+}
+  
+function file_explorer() {
+    document.getElementById('selectfile').click();
+    document.getElementById('selectfile').onchange = function() {
+        fileobj = document.getElementById('selectfile').files[0];
+        ajax_file_upload(fileobj);
+    };
+}
+  
+function ajax_file_upload(file_obj) {
+    if(file_obj != undefined) {
+        var form_data = new FormData();                  
+        form_data.append('file', file_obj);
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("POST", "ajax.php", true);
+        xhttp.onload = function(event) {
+            oOutput = document.querySelector('.img-content');
+            if (xhttp.status == 200) {
+                oOutput.innerHTML = "<img src='"+ this.responseText +"' alt='The Image' />";
+            } else {
+                oOutput.innerHTML = "Error " + xhttp.status + " occurred when trying to upload your file.";
+            }
+        }
+ 
+        xhttp.send(form_data);
+    }
+}
